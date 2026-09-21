d21155129311da8ed6eb42d6fb2e2fcd21c44186
Chandu_Nissankuni <chandunissankuni0143@gmail.com>
2026-09-21 23:31:39 +0530

    Prepare backend for production deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const helmet = require("helmet");
 const app = express();
 app.use(cors());
 app.use(helmet());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 // Middleware
@@ -46,6 +46,6 @@ app.get("/test-db", async (req, res) => {
 
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
